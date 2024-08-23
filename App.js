80c8fe8e6c904bf28db8720b6a8fb3da8dc50a71
Emir <emirmarash@gmail.com>
2024-08-23 20:45:35 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import Home from "./Home";
+import Page2 from "./Page2";
+import Page1 from "./Page1";
+import {BrowserRouter,  Routes, Route,Link} from 'react-router-dom'
+function App() {
+  return(
+    <div className="App" style={{margin:0,padding:0,boxSizing:"border-box"}}>
+      <BrowserRouter>
+      <header style={{backgroundColor:"grey",position:"relative",padding:40,textAlign:"center",border:2,borderRadius:20 }}>
+        <Link style={{color:"white"}} to='/'>{<h1>Home</h1>}</Link>
+        <Link style={{color:"white"}} to='/page1'>{<h1>Page1</h1>}</Link>
+        <Link style={{color:"white"}} to='/page2'>{<h1>Page2</h1>}</Link>
+     
+      </header>
+      <Routes>
+
+        <Route path="/" element={<Home/>} />
+        <Route path="/page1" element={<Page1/>}/>
+        <Route path="/page2" element={<Page2/>}/>
+
+      </Routes>
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
