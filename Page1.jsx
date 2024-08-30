6d0731a4c6c64f4bd674a4071b1511151cd047cb
Emir <emirmarash@gmail.com>
2024-08-30 17:31:55 +0600

    Update Page1.jsx

--- Page1.jsx
+++ Page1.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Page1 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const Home = () => {
+  const [users, setUsers] = useState([]);
 
-export default Page1;
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+        const data = await response.json();
+        setUsers(data); 
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+      }
+    };
+
+    fetchUsers(); 
+  }, []); 
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Home</h1>
+      {users.map(user => (
+        <h1 key={user.id}>{user.company.catchPhrase}</h1> 
+      ))}
+    </div>
+  );
+};
+
+export default Home;
